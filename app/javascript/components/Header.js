30fc95f91c3c4de9c210d7b73b82b940ef3713f2
gowthamchinna <56022993+gowthamchinna@users.noreply.github.com>
2019-11-25 18:19:34 +0530

    Update Header.js

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = () => (
   <header>
     <h1>
-      <Link to="/events">Event Manager - V2</Link>
+      <Link to="/events">Event Manager - POC (MSys)</Link>
     </h1>
   </header>
 );

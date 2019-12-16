e5a300cbad3f5145efe4752940d7e042947b4ef9
Vijay Mali <43739267+vijaymmali1990@users.noreply.github.com>
2019-12-16 16:40:45 +0530

    Update Header.js

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = () => (
   <header>
     <h1>
-      <Link to="/events">Coupa POC Application (MSys)</Link>
+      <Link to="/events">Test demo</Link>
     </h1>
   </header>
 );
